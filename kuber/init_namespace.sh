6b3b84d386b670492a4c34623615ba466f5f77ca
Максим Попов <mi69popov@gmail.com>
2024-07-09 02:43:12 +0300

    Fix

--- kuber/init_namespace.sh
+++ kuber/init_namespace.sh
@@ -10,6 +10,6 @@ NAMESPACE=popovmi-tictactoe-$STAGE
 
 kubectl create namespace $NAMESPACE
 
-sed "s/{{NAMESPACE}}/$NAMESPACE/g" volume.yaml | kubectl apply -f -
-sed -e "s/{{NAMESPACE}}/$NAMESPACE/g" -e "s~{{SERVER_IMAGE}}~popovmi/tictactoe-server~g" server.yaml | kubectl apply -f -
-sed "s/{{NAMESPACE}}/$NAMESPACE/g" ingress.yaml | kubectl apply -f -
+sed "s/{{NAMESPACE}}/$NAMESPACE/g" ./volume.yaml | kubectl apply -f -
+sed -e "s/{{NAMESPACE}}/$NAMESPACE/g" -e "s~{{SERVER_IMAGE}}~popovmi/tictactoe-server~g" ./server.yaml | kubectl apply -f -
+sed "s/{{NAMESPACE}}/$NAMESPACE/g" ./ingress.yaml | kubectl apply -f -
