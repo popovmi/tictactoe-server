08038ec9cd32640501626e08faa29b5d2c1a4aa6
Максим Попов <mi69popov@gmail.com>
2024-07-09 02:47:58 +0300

    Naming

--- kuber/init_namespace.sh
+++ kuber/init_namespace.sh
@@ -10,6 +10,6 @@ NAMESPACE=popovmi-tictactoe-$STAGE
 
 kubectl create namespace $NAMESPACE
 
-sed "s/{{NAMESPACE}}/$NAMESPACE/g" kuber/volume.yaml | kubectl apply -f -
-sed -e "s/{{NAMESPACE}}/$NAMESPACE/g" -e "s~{{SERVER_IMAGE}}~popovmi/tictactoe-server~g" kuber/server.yaml | kubectl apply -f -
-sed "s/{{NAMESPACE}}/$NAMESPACE/g" kuber/ingress.yaml | kubectl apply -f -
+sed "s/{{NAMESPACE}}/$NAMESPACE/g" kuber/volume.yml | kubectl apply -f -
+sed -e "s/{{NAMESPACE}}/$NAMESPACE/g" -e "s~{{SERVER_IMAGE}}~popovmi/tictactoe-server~g" kuber/server.yml | kubectl apply -f -
+sed "s/{{NAMESPACE}}/$NAMESPACE/g" kuber/ingress.yml | kubectl apply -f -
