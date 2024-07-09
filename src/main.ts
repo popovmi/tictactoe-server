7b9df90b032a718ddb24b9a7db7311ed5af90bba
Максим Попов <mi69popov@gmail.com>
2024-07-09 10:01:57 +0300

    Add game module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import cors from 'cors';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const port = process.env.PORT || 8080;
+
+  app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+    }),
+  );
+
+  await app.listen(port, '0.0.0.0', () => {
+    console.log(`Application is running on: http://0.0.0.0:${port}`);
+  });
 }
 bootstrap();
