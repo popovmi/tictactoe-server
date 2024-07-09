0a6da9d860eaba8319ecc1b58dfd03d0e31f7679
Максим Попов <mi69popov@gmail.com>
2024-07-09 11:01:23 +0300

    Add log

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const port = process.env.PORT || 8080;
-
+  console.log('ORIGIN: ' + process.env.CORS_ORIGIN);
   app.use(
     cors({
       origin: process.env.CORS_ORIGIN,
