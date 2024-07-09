54b1878b0f4a11271ff0c10e4a97e258bad85249
Максим Попов <mi69popov@gmail.com>
2024-07-09 12:13:00 +0300

    Fix

--- kuber/deploy_server.sh
+++ kuber/deploy_server.sh
@@ -12,6 +12,6 @@ NAMESPACE=popovmi-tictactoe-$STAGE
 sed "s/{{NAMESPACE}}/$NAMESPACE/g" kuber/volume.yml | kubectl apply -f -
 sed -e "s/{{NAMESPACE}}/$NAMESPACE/g" \
   -e "s~{{SERVER_IMAGE}}~$SERVER_DOCKER_IMAGE:$CI_COMMIT_SHORT_SHA~g" \
-  # -e "s/{{CORS_ORIGIN}}/$CORS_ORIGIN/g" \
+  -e "s/{{CORS_ORIGIN}}/$CORS_ORIGIN/g" \
   kuber/server.yml | kubectl apply -f -
 sed "s/{{NAMESPACE}}/$NAMESPACE/g" kuber/ingress.yml | kubectl apply -f -
