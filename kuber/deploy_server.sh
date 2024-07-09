1bcc37897de8d429d1134e73412970bcd1277835
Максим Попов <mi69popov@gmail.com>
2024-07-09 15:00:27 +0300

    Update hostname config

--- kuber/deploy_server.sh
+++ kuber/deploy_server.sh
@@ -3,10 +3,15 @@ ENVIRONMENT=$1
 echo "Prepare deployment for env: $ENVIRONMENT"
 
 NAMESPACE=popovmi-tictactoe-$ENVIRONMENT
+HOSTNAME=$NAMESPACE.localtest.me
 
 sed "s/{{NAMESPACE}}/$NAMESPACE/g" kuber/volume.yml | kubectl apply -f -
+
 sed -e "s/{{NAMESPACE}}/$NAMESPACE/g" \
   -e "s~{{SERVER_IMAGE}}~$SERVER_DOCKER_IMAGE:$CI_COMMIT_SHORT_SHA~g" \
   -e "s/{{CORS_ORIGIN}}/$CORS_ORIGIN/g" \
   kuber/server.yml | kubectl apply -f -
-sed "s/{{NAMESPACE}}/$NAMESPACE/g" kuber/ingress.yml | kubectl apply -f -
+
+sed -e "s/{{NAMESPACE}}/$NAMESPACE/g" \
+  -e "s/{{HOSTNAME}}/$HOSTNAME/g" \
+  kuber/ingress.yml | kubectl apply -f -
