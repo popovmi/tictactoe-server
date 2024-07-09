5ab7186db4e1c4c17eff005e757ad4ae2c81a5e4
Максим Попов <mi69popov@gmail.com>
2024-07-09 10:45:38 +0300

    Update dynamic pipeline

--- kuber/generate-gitlab-ci.js
+++ kuber/generate-gitlab-ci.js
@@ -1,20 +1,9 @@
 const { writeFileSync } = require('fs');
 
 const branch = process.env.CI_COMMIT_BRANCH;
-
 const environment = branch === 'main' ? 'prod' : branch;
 
-const gitlabCI = `image: docker:stable
-services:
-# we will build our images by running docker daemon inside a container
-- docker:dind
-variables:
-  DOCKER_DRIVER: overlay2
-  SERVER_DOCKER_IMAGE: popovmi/tictactoe-server
-
-stages:
-- deploy
-
+const gitlabCI = `
 deploy_server:
   stage: deploy
   environment: ${environment}
