6d0e2c3b06932a119aad0fd9f26bfbd6565410a2
Максим Попов <mi69popov@gmail.com>
2024-07-09 12:00:28 +0300

    Update gitlab ci

--- kuber/deploy_server.sh
+++ kuber/deploy_server.sh
@@ -5,10 +5,12 @@ if [[ $STAGE == "main" ]]; then
 fi
 
 echo "STAGE: $STAGE"
-echo "CORS_ORIGIN: $CORS_ORIGIN"
 
 NAMESPACE=popovmi-tictactoe-$STAGE
 
 sed "s/{{NAMESPACE}}/$NAMESPACE/g" kuber/volume.yml | kubectl apply -f -
-sed -e "s/{{NAMESPACE}}/$NAMESPACE/g" -e "s~{{SERVER_IMAGE}}~$SERVER_DOCKER_IMAGE:$CI_COMMIT_SHORT_SHA~g" kuber/server.yml | kubectl apply -f -
+sed -e "s/{{NAMESPACE}}/$NAMESPACE/g" \
+  -e "s~{{SERVER_IMAGE}}~$SERVER_DOCKER_IMAGE:$CI_COMMIT_SHORT_SHA~g" \
+  -e "s/{{CORS_ORIGIN}}/$CORS_ORIGIN/g" \
+  kuber/server.yml | kubectl apply -f -
 sed "s/{{NAMESPACE}}/$NAMESPACE/g" kuber/ingress.yml | kubectl apply -f -
